de6829870a88b4080567b61e07c7f0e69c95e000
ArthasMenethil-A <arvin1844m@gmail.com>
2022-07-16 11:34:41 +0430

    added the class of Inverse kinematics

--- trajectory_planning.py
+++ trajectory_planning.py
@@ -12,17 +12,34 @@
 import numpy as np 
 import math 
 
-
-# =================================================================================================
-# -- forward kinematic class ----------------------------------------------------------------------
-# =================================================================================================
-
-
-
-
-
 # =================================================================================================
 # -- inverse kinematic class ----------------------------------------------------------------------
 # =================================================================================================
+
+# here we have the EE position and the lengths of rods and basic geometry
+# our goal is to find theta1, theta2 and theta3 
+
+class InverseKinematics:
+	def __init__(self, EE_position, active_rod, passive_rod, base_radius, EE_raidus):
+		# initializing the basic geometry and the given data
+
+		self.EE_position = np.array(EE_position)	# end effoctor position (x_e, y_e, z_e)
+		self.active_rod = active_rod		# length of the active rod (the upper rod or r_f)
+		self.passive_rod = passive_rod		# length of the passive rod (the lower rod or r_e)
+		self.EE_raidus = EE_raidus			# the radius of the end effoctor e
+		self.base_radius = base_radius		# the radius of the base or f
+
+	def get_positions(self):
+		# here positions of the the 3 important points are found E1, E1_prime, F1
+		# and from those we find J1
+		self.E1_position = 
+
+# =================================================================================================
+# -- main -----------------------------------------------------------------------------------------
+# =================================================================================================
+
+
+Kinematics = InverseKinematics([-0.5, 0, 0], 0.3, 0.5, 0.3, 0.1)
+
 
 
