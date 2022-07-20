9e91c3352faa70fd0795243faa3c8043f1242ce3
ArthasMenethil-A <arvin1844m@gmail.com>
2022-07-20 12:51:20 +0430

    completed the 4567 method
    point to point movement is now complete

--- trajectory_planning_4567.py
+++ trajectory_planning_4567.py
@@ -1,5 +1,4 @@
-# attempt #1 
-# trajectory planning, point to point movement with 5rd order polynomial 
+# trajectory planning, point to point movement with 7th order polynomial 
 # 2 fundamental questions: 
 # 1. given a specific location in the real world, what values should my robot's joint be ...
 # ... set to in order to get the EE there? (inverse kinematics)
@@ -10,14 +9,15 @@
 # =================================================================================================
 
 import time
-
 t = time.time()
 
 import numpy as np 
 import math 
 import matplotlib.pyplot as plt 
+
 print("time is:")
 print(time.time() - t)
+
 # =================================================================================================
 # -- inverse kinematic class ----------------------------------------------------------------------
 # =================================================================================================
@@ -82,8 +82,6 @@ class InverseKinematics:
 			z = c1 + c2*y
 
 			self.J1_position[i] = [0, y, z]
-
-		
 	
 	def get_theta(self):
 		self.theta = [0, 0, 0]
@@ -93,125 +91,119 @@ class InverseKinematics:
 			y_F1 = self.F1_position[i][1]
 
 			self.theta[i] = (math.atan(-z_J1/(y_F1 - y_J1)))
-		print(" this is default theta: ")
-		print(np.array(self.theta)*180/math.pi)
 
 		# Gearbox ratio = 50, zero offset = 47.2 degree
 		self.theta = ((np.array(self.theta)*180/math.pi) + 47.2)*50
-		print(" this is theta with GR consideration")
-		
-		
+
 		return self.theta
 
 # =================================================================================================
-# -- point to point movement 3-4-5 ----------------------------------------------------------------
+# -- point to point movement 4-5-6-7 ----------------------------------------------------------------
 # =================================================================================================
 
-# in this part we want to move the End-Effector from 1 point to another using a 3-4-5 technique 
-# in this class the Given Data is: the first and final position of EE, the max value of V
+# in this part we want to move the End-Effector from 1 point to another using a 4-5-6-7 technique 
+# in this class the Given Data is: the first and final position of EE, the max value of theta_dot
 # and we give the, Desired Data: velocity profile
 
-class PointToPoint345Movement:
+class PointToPoint4567Movement:
 	def __init__(self,EE_position_i, EE_position_f, theta_max=4050):
 		self.EE_position_i = EE_position_i
 		self.EE_position_f = EE_position_f
-		self.theta_max = theta_max*6		# by defaut = 424.115 rad/s = 4050 rpm
-		self.theta_i = np.zeros((3, 1))
-		self.theta_f = np.zeros((3, 1))
-
-
+		self.theta_max = theta_max*6		# by defaut = 4050 rpm = 4050*6 deg/s
+		self.theta_i = np.zeros((3, 1))		# initializing the theta_i 
+		self.theta_f = np.zeros((3, 1))		# initializing the theta_f
+	
 	def inverse_kinematics(self):
-
 		# here we find the Initial and final theta from the Initial and final EE position 
+
+		# Given data of the robot geometry 
 		active_rod = 0.2
 		passive_rod = 0.46
 		base_radius = 0.3464101615 
 		EE_radius = 0.2563435195 
 
+		# initializing the InverseKinematics class
 		Kinematics = InverseKinematics(self.EE_position_i, active_rod, passive_rod, base_radius, EE_radius)
 		Kinematics.get_J1_positions()
 
+		# getting the theta_i from position_i
 		theta_i = Kinematics.get_theta()
 		
-		
+		# getting the theta_f from position_f
 		Kinematics.EE_position_global = self.EE_position_f
 		Kinematics.get_J1_positions()
 		theta_f = Kinematics.get_theta()
 
+		# reshape
 		for i in [0, 1, 2]:
 			self.theta_i[i] = theta_i[i]
 			self.theta_f[i] = theta_f[i]
 
-		print("this is the initial theta")
-		print(self.theta_i)
-		print(self.theta_i.size)
-		print("this is the final theta")
-		print(self.theta_f)
-		print(self.theta_f.size)
-
 	def T(self):
 		# here we find T(period) from the theta_max
-		self.T = 15/8*np.array(self.theta_f - self.theta_i)/self.theta_max
+		self.T = 35/16*np.array(self.theta_f - self.theta_i)/self.theta_max
 		self.T = max(self.T)
 
-		print("this is T")
-		print(self.T)
-
+		#print("this is T")
+		#print(self.T)
 
 	def theta_dot_t(self):
 		# we find the angular velocity profile in this part
 		T = math.floor(self.T*1000)
 		tau = np.array(range(0, T))/T
-		s_tau_d = 30*tau**4 - 60*tau**3 + 30*tau**2
-		print("this is size of s matrix")
-		print(s_tau_d)
-
+		s_tau_d = -140*tau**6 + 420*tau**5 - 420*tau**4 + 140*tau**3
 		self.theta_dot_t = np.array(self.theta_f - self.theta_i)/self.T*s_tau_d
-		print(self.theta_dot_t.shape)
-		print("this is theta dot t")
-		print(self.theta_dot_t)
 
 	def theta_t(self):
 		# we find the angular position profile in this part
 		T = math.floor(self.T*1000)
 		tau = np.array(range(0, T))/T
-		s_tau = 6*tau**5 - 15*tau**4 + 10*tau**3
-
+		s_tau = -20*tau**7 + 70*tau**6 - 84*tau**5 + 35*tau**4
 		self.theta_t = np.array(self.theta_i) + np.array(self.theta_f - self.theta_i)*s_tau
-		print(self.theta_t.shape)
-		print("this is theta t")
-		print(self.theta_t)
+
 
 # =================================================================================================
 # -- main -----------------------------------------------------------------------------------------
 # =================================================================================================
 
+#
 
-movement = PointToPoint345Movement([0.05, 0.05, -0.31], [0, -0.15, -0.42])
+# initializing the point to point movement class 
+movement = PointToPoint4567Movement([0, 0.15, -0.4], [0.05, 0, -0.40], theta_max=4050*0.1)
 movement.inverse_kinematics()
+
+# over writing the theta_i to be zero
+for i in [0, 1, 2]:
+	movement.theta_i[i] = 47.2*50
+
+print(" this is final theta: " + str(movement.theta_f))
 movement.T()
 movement.theta_dot_t()
 movement.theta_t()
 
+# the calculations take about 1 ms 
+print("time is:")
+print(time.time() - t)
+
+# plotting the calculated data
 T = math.floor(movement.T*1000)
+print("this is T: " + str(T))
 tau = np.array(range(0, T))/T
 plt.figure()
 plt.plot(tau, movement.theta_t.transpose())
 plt.figure()
-plt.plot(tau, movement.theta_dot_t.transpose())
+plt.plot(tau, movement.theta_dot_t.transpose()/6)
 
+plt.show()
+# converting the data to rpm
 theta_t = movement.theta_t/6
 theta_dot_t = movement.theta_dot_t/6
 
-f = open("E:\\PointToPoint345.h", "a")
-f.write("float speeds_motor1[] = { 0")
-for i in range(1, T):
-	f.write(", " + str(theta_t[0][i])[:7])
-f.write("\nfloat speeds_motor2[] = { 0")
-
-for i in range(1, T):
-	f.write(", " + str(theta_t[1][i])[:7])
-f.write("\nfloat speeds_motor3[] = { 0")
-
-for i in range(1, T):
-	f.write(", " + str(theta_t[2][i])[:7])
+# writing to file 
+myfile = open("E:\\PointToPoint4567.h", "a")
+list1 = theta_dot_t.tolist()
+line = ["", "", ""]
+for j in [0, 1, 2]:
+	list1[j] = [round(float(i), 4) for i in list1[j]]
+	line[j] = ','.join(str(e) for e in list1[j])
+	myfile.write("float speeds_motor" + str(j+1) + "[] = {" + line[j] + "};\n" )
