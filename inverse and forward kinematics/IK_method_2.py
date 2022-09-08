df890cb57044ce06eeeea50d6353093e9e5213a2
ArthasMenethil-A <arvin1844m@gmail.com>
2022-09-09 00:42:11 +0430

    Update IK_method_2.py

--- inverse and forward kinematics/IK_method_2.py
+++ inverse and forward kinematics/IK_method_2.py
@@ -8,9 +8,10 @@
 
 import numpy as np 
 import math 
-from math import sin, cos, acos, asin
+from math import sin, cos, acos, asin, atan
 from numpy.linalg import inv
 import matplotlib.pyplot as plt 
+from numpy import matmul
 import time
 
 # =================================================================================================
@@ -18,15 +19,15 @@ import time
 # =================================================================================================
 
 class Jacobian:
-	def __init__(self, EE_position, active_rod=0.2, passive_rod=0.46, base_triangle_side=0.3464101615, EE_triangle_side=0.2563435195, alpha=[90, 210, 330]):
+	def __init__(self, EE_position, active_rod=0.2, passive_rod=0.46, base_triangle_side=0.3464101615, EE_triangle_side=0.2563435195, alpha=[0, 120, 240]):
 		
 		# initializing the basic geometry and the given data
 		self.alpha = np.array(alpha)							# alpha angles
 		self.EE_position_global = np.array(EE_position)			# end effoctor position (x_e, y_e, z_e) with respect to alpha1 = 0								
 		self.active_rod = active_rod							# length of the active rod (the upper rod or r_f)
 		self.passive_rod = passive_rod							# length of the passive rod (the lower rod or r_e)
-		self.EE_triangle_side = EE_triangle_side								# the radius of the end effoctor e 
-		self.base_triangle_side = base_triangle_side							# the radius of the base or f
+		self.EE_triangle_side = EE_triangle_side				# the triangle side of the end effoctor e 
+		self.base_triangle_side = base_triangle_side			# the triangle side of the base or f
 
 	def inverse_kinematics(self): 
 		# this also calculate inverse kinematic
@@ -58,22 +59,23 @@ class Jacobian:
 			A = px*cos(alpha[i]) - py*sin(alpha[i]) - R + r
 			B = pz
 			M = -(A**2 + B**2 + a**2 - (b*sin(theta_3[i]))**2)/(2*a)
-			t = (-A + (A**2 + B**2 - M**2)**0.5)/(M - B)
-			theta_1[i] = 2*math.atan(t)
-			theta_2[i] = acos((A - a*cos(theta_1[i]))/(b*sin(theta_3[i]))) - theta_1[i]
+			t = (-B + (A**2 + B**2 - M**2)**0.5)/(M - A)
+			theta_1[i] = 2*atan(t)
+			theta_2[i] = asin((pz - a*sin(theta_1[i]))/(b*sin(theta_3[i]))) - theta_1[i]
+
 		
 		theta_1 *= 180/math.pi
 		theta_2 *= 180/math.pi
 		theta_3 *= 180/math.pi
 
 		theta_1_new = np.zeros((3))
-		theta_1_new[0] = theta_1[0] - 90 # need this for writing the file 
-		theta_1_new[1] = theta_1[2] - 90
-		theta_1_new[2] = theta_1[1] - 90
+		theta_1_new[0] = theta_1[0]  
+		theta_1_new[1] = theta_1[1] 
+		theta_1_new[2] = theta_1[2] 
 
 		return (theta_1, theta_2, theta_3)
 	
-	def get_jacobian_matrix(self, theta_1, thetea_2, theta_3): 
+	def get_jacobian_matrix(self, theta_1, theta_2, theta_3): 
 		
 		# changing the angles back to rad
 		theta_1 /= 180/math.pi
@@ -109,44 +111,36 @@ class Jacobian:
 		theta_2 = theta[1]
 		theta_3 = theta[2]
 
-
 		px = cos(alpha)*(R - r + a*cos(theta_1) + b*sin(theta_3)*cos(theta_2 + theta_1)) + sin(alpha)*b*cos(theta_3)
 		py = sin(alpha)*(r - R - a*cos(theta_1) - b*sin(theta_3)*cos(theta_2 + theta_1)) + cos(alpha)*b*cos(theta_3)
 		pz = a*sin(theta_1) + b*sin(theta_3)*sin(theta_2 + theta_1) # not correct why ???
 		
-		print(px, py, pz)
 		return (px, py, pz)
+
+	def theta_dot(self, j_p, j_theta, v):
+		return matmul(matmul(inv(j_theta), j_p), v)
+
+
 # =================================================================================================
 # -- TEST ----------------------------------------------------------------------
 # =================================================================================================
-t = time.time()
 
-jacobian = Jacobian([0.05, 0.05, -0.31])
-# print(jacobian.inverse_kinematics())
-temp_theta = np.array(jacobian.inverse_kinematics()).transpose()[0]
-# print(temp_theta)
-jacobian.forward_kinematics(temp_theta)
+t = np.linspace(0, 1, 1000)
+gamma = t*2*np.pi 
+x = 0.1*np.cos(gamma)
+y = 0.1*np.sin(gamma)
+z = -0.35
 
-jacobian = Jacobian([-0.05, 0.05, -0.40])
-print(jacobian.inverse_kinematics()[0])
-temp_theta = np.array(jacobian.inverse_kinematics()).transpose()[0]
-# print(temp_theta)
-jacobian.forward_kinematics(temp_theta)
+x_dot = 0.1*2*np.pi*(-np.sin(gamma))
+y_dot = 0.1*2*np.pi*(np.cos(gamma))
 
-jacobian = Jacobian([-0.10, -0.10, -0.42])
-print(jacobian.inverse_kinematics()[0])
-temp_theta = np.array(jacobian.inverse_kinematics()).transpose()[0]
-# print(temp_theta)
-jacobian.forward_kinematics(temp_theta)
 
-jacobian = Jacobian([0.0, -0.15, -0.42])
-print(jacobian.inverse_kinematics()[0])
-temp_theta = np.array(jacobian.inverse_kinematics()).transpose()[0]
-# print(temp_theta)
-jacobian.forward_kinematics(temp_theta)
+THETA_DOT = np.zeros((1000, 3))
+for i, t_i in enumerate(t):
+	jacobian = Jacobian([x[i], y[i], z])
+	theta_1, theta_2, theta_3 = jacobian.inverse_kinematics()
+	j_p, j_theta = jacobian.get_jacobian_matrix(theta_1, theta_2, theta_3)
+	THETA_DOT[i, :] = jacobian.theta_dot(j_p, j_theta, [x_dot[i], y_dot[i], 0])
 
-jacobian = Jacobian([-0.15, 0.1, -0.42])
-print(jacobian.inverse_kinematics()[0])
-temp_theta = np.array(jacobian.inverse_kinematics()).transpose()[0]
-# print(temp_theta)
-jacobian.forward_kinematics(temp_theta)
+plt.plot(t, THETA_DOT[:, :])
+plt.show()
